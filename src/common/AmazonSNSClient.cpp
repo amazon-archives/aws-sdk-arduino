97dfef4e58e36a90840af91eb1ea2d2998d944c4
Rohan Deshpande <deshpan@amazon.com>
2014-11-02 18:23:37 -0800

    AmazonSNSClient derives from AWSClient2, which handles AWS/Query responses. The updated client also extracts and uses the timestamp in the AWS response.

--- src/common/AmazonSNSClient.cpp
+++ src/common/AmazonSNSClient.cpp
@@ -1,104 +1,135 @@
 #include "AmazonSNSClient.h"
 #include "AWSFoundationalTypes.h"
 #include <stdlib.h>
-#include <application.h>
+#include "Utils.h"
+
+static const char* SERVICE = "sns";
+static const char* FORM_TYPE = "application/x-www-form-urlencoded";
+static const char* PAYLOAD_TEMPLATE = "Action=Publish&TargetArn=%s&Message=%s&Version=2010-03-31\n";
+int PAYLOAD_TEMPLATE_LENGTH = 54;
+int MESSAGEID_BUFFER_LENGTH = 37;
+int EXTRACTED_TIMESTAMP_BUFFER_LENGTH = 17;
+int FORMATTED_TIMESTAMP_BUFFER_LENGTH = 15;
 
 PublishInput::PublishInput() {
-	reset();
+    reset();
 }
 
 void PublishInput::reset() {
-	targetArnBeenSet = false;
-	messageBeenSet = false;
+    targetArnBeenSet = false;
+    messageBeenSet = false;
 }
 
 bool PublishInput::requiredAreSet() const {
-	return targetArnBeenSet && messageBeenSet;
+    return targetArnBeenSet && messageBeenSet;
 }
 
 void PublishInput::setTargetArn(MinimalString targetArn) {
-	targetArnBeenSet = true;
-	this->targetArn = targetArn;
+    targetArnBeenSet = true;
+    this->targetArn = targetArn;
 }
 
 void PublishInput::setMessage(MinimalString message) {
-	messageBeenSet = true;
-	this->message = message;
+    messageBeenSet = true;
+    this->message = message;
 }
 
 MinimalString PublishInput::getTargetArn() const {
-	return this->targetArn;
+    return this->targetArn;
 }
 
 MinimalString PublishInput::getMessage() const {
-	return this->message;
+    return this->message;
 }
 
 MinimalString PublishInput::serialize() const {
-	char* payload;
+    char* payload = new char[PAYLOAD_TEMPLATE_LENGTH + message.length() + targetArn.length() + 1]();
 	
-	sprintf(payload, "Action=Publish&TargetArn=%s&Message=%s", targetArn.getCStr(), message.getCStr());
+    sprintf(payload, PAYLOAD_TEMPLATE, targetArn.getCStr(), message.getCStr());
 	
-	return MinimalString(payload);
+    return MinimalString(payload);
 }
 
 PublishOutput::PublishOutput() {
-	reset();
+    reset();
 }
 
 void PublishOutput::reset() {
-	messageIdBeenSet = false;
+    messageIdBeenSet = false;
 	
-	errorType = MinimalString();
-	errorMessage = MinimalString();
+    errorType = MinimalString();
+    errorMessage = MinimalString();
 }
 
 void PublishOutput::setMessageId(MinimalString messageId) {
-	messageIdBeenSet = true;
-	this->messageId = messageId;
+    messageIdBeenSet = true;
+    this->messageId = messageId;
 }
 
 MinimalString PublishOutput::getMessageId() const {
-	return this->messageId;
+    return this->messageId;
 }
 
 MinimalString PublishOutput::getErrorType() const {
-	return errorType;
+    return errorType;
 }
 
 MinimalString PublishOutput::getErrorMessage() const {
-	return errorMessage;
+    return errorMessage;
 }
 
-AmazonSNSClient::AmazonSNSClient() : AWSClient() {
+AmazonSNSClient::AmazonSNSClient() : AWSClient2() {
+    awsService = SERVICE;
 }
 
 PublishOutput AmazonSNSClient::publish(PublishInput publishInput, ActionError& actionError) {
-	actionError = NONE_ACTIONERROR;
+    actionError = NONE_ACTIONERROR;
 
-	PublishOutput publishOutput;
+    PublishOutput publishOutput;
 	
-	if (!publishInput.requiredAreSet()) {
-		actionError = MISSING_REQUIRED_ARGS_ACTIONERROR;
-		return publishOutput;
-	}
-	
-	httpS = false;
-	MinimalString payload = publishInput.serialize();
-    char* request;
-    if (httpClient->usesCurl()) {
-        request = createCurlRequest(payload);
-    } else {
-        request = createRequest(payload);
+    if (!publishInput.requiredAreSet()) {
+        actionError = MISSING_REQUIRED_ARGS_ACTIONERROR;
+	return publishOutput;
     }
-    Serial.println(request);
+	
+    contentType = FORM_TYPE;
+    MinimalString payload = publishInput.serialize();
+	
+    char* request = createRequest(payload);
     char* response = sendData(request);
     delete[] request;
+    
     if (response == NULL) {
         actionError = CONNECTION_ACTIONERROR;
         return publishOutput;
-    }	
-    Serial.println(response);
+    }
 
-	return publishOutput;
+    int httpStatusCode = findHttpStatusCode(response);
+	
+    if (httpStatusCode == 200) {
+        char* msgidIdx = strstr(response, "<MessageId>");
+        int msgidPos = msgidIdx - response;
+
+        char* msgid = new char[MESSAGEID_BUFFER_LENGTH]();
+        strncpy(msgid, response + msgidPos + 11, MESSAGEID_BUFFER_LENGTH - 1);
+        msgid[37] = '\0';
+		
+        publishOutput.setMessageId(msgid);
+        return publishOutput;
+    }
+    
+    if (httpStatusCode == 403) {
+        char* ts = strstr(response, "earlier than ");
+        int pos = ts - response;
+    	
+        char* newts = new char[EXTRACTED_TIMESTAMP_BUFFER_LENGTH]();
+        strncpy(newts, response + pos + 31, EXTRACTED_TIMESTAMP_BUFFER_LENGTH - 1);
+        newts[16] = '\0';
+    	
+        char* time = new char[FORMATTED_TIMESTAMP_BUFFER_LENGTH]();
+        sprintf(time, "%.8s%.6s", newts, newts + 9);
+        dateTimeProvider->sync(time);
+    }
+    
+    return publishOutput;
 }
--- src/common/AmazonSNSClient.h
+++ src/common/AmazonSNSClient.h
@@ -1,39 +1,39 @@
 #ifndef AMAZONSNSCLIENT_H_
 #define AMAZONSNSCLIENT_H_
 #include "AWSFoundationalTypes.h"
-#include "AWSClient.h"
+#include "AWSClient2.h"
 
 class PublishInput {
-	MinimalString targetArn;
-	MinimalString message;
-	bool targetArnBeenSet;
-	bool messageBeenSet;
-	void reset();
+    MinimalString targetArn;
+    MinimalString message;
+    bool targetArnBeenSet;
+    bool messageBeenSet;
+    void reset();
 public:
-	PublishInput();
-	bool requiredAreSet() const;
-	void setMessage(MinimalString message);
-	void setTargetArn(MinimalString targetArn);
-	MinimalString getMessage() const;
-	MinimalString getTargetArn() const;
-	MinimalString serialize() const;
+    PublishInput();
+    bool requiredAreSet() const;
+    void setMessage(MinimalString message);
+    void setTargetArn(MinimalString targetArn);
+    MinimalString getMessage() const;
+    MinimalString getTargetArn() const;
+    MinimalString serialize() const;
 };
 
 class PublishOutput {
-	MinimalString messageId;
-	bool messageIdBeenSet;
-	MinimalString errorType;
-	MinimalString errorMessage;
-	void reset();
+    MinimalString messageId;
+    bool messageIdBeenSet;
+    MinimalString errorType;
+    MinimalString errorMessage;
+    void reset();
 public:
-	PublishOutput();
+    PublishOutput();
     MinimalString getErrorType() const;
     MinimalString getErrorMessage() const;
     void setMessageId(MinimalString messageId);
     MinimalString getMessageId() const;
 };
 
-class AmazonSNSClient : public AWSClient {
+class AmazonSNSClient : public AWSClient2 {
 public:
     AmazonSNSClient();
     PublishOutput publish(PublishInput input, ActionError& actionError);
