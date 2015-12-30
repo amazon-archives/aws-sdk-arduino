3b7d7ad54d686642f42bc301b007303199738ec5
Sander van de Graaf <mail@svdgraaf.nl>
2015-12-30 01:06:18 +0100

    cleanup, and use correct signing headers

--- src/common/AmazonIOTClient.cpp
+++ src/common/AmazonIOTClient.cpp
@@ -6,7 +6,7 @@
 AmazonIOTClient::AmazonIOTClient() : AWSClient4() {
     this->awsService = "iotdata";
     this->contentType = "application/json";
-    this->signedHeaders = "host;x-amz-content-sha256;x-amz-date";
+    this->signedHeaders = "content-type;host;x-amz-content-sha256;x-amz-date";
     this->uri = "/";
     this->queryString = "";
     this->httpS = true;
@@ -17,36 +17,7 @@ char* AmazonIOTClient::update_shadow(MinimalString shadow, ActionError& actionEr
 
     this->method = "POST";
     char* request = createRequest(shadow);
+    // return request;
     char* response = sendData(request);
     return response;
-
-
-    // delete[] request;
-    //
-    // if (response == NULL) {
-    //     actionError = CONNECTION_ACTIONERROR;
-    //     return 999;
-    // }
-    //
-    // int httpStatusCode = findHttpStatusCode(response);
-    //
-    // if (httpStatusCode == 200) {
-    //   return 1;
-    // }
-    //
-    // if (httpStatusCode == 403) {
-    //     char* ts = strstr(response, "earlier than ");
-    //     int pos = ts - response;
-    //
-    //     char* newts = new char[IOT_EXTRACTED_TIMESTAMP_BUFFER_LENGTH]();
-    //     strncpy(newts, response + pos + 31, IOT_EXTRACTED_TIMESTAMP_BUFFER_LENGTH - 1);
-    //     newts[16] = '\0';
-    //
-    //     char* time = new char[IOT_FORMATTED_TIMESTAMP_BUFFER_LENGTH]();
-    //     sprintf(time, "%.8s%.6s", newts, newts + 9);
-    //     dateTimeProvider->sync(time);
-    //     return 0;
-    // }
-    //
-    // return httpStatusCode;
 }
